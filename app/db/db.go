5423c71ddf674bd2994d40e9edfdadc85777cb24
YutoUrushima <yuto259233@gmail.com>
2023-07-30 21:17:11 +0900

    Fix register

--- app/db/db.go
+++ app/db/db.go
@@ -9,7 +9,7 @@ import (
 )
 
 func OpenDB() (*sql.DB, error) {
-	dsn := os.Getenv("DB_USERNAME") + ":" + os.Getenv("DB_PASSWORD") + "@/tcp(" + os.Getenv("DB_HOST") + ":" + os.Getenv("DB_PORT") + ")/" + os.Getenv("DB_NAME") + "?tls=true"
+	dsn := os.Getenv("DB_USERNAME") + ":" + os.Getenv("DB_PASSWORD") + "@tcp(" + os.Getenv("DB_HOST") + ":" + os.Getenv("DB_PORT") + ")/" + os.Getenv("DB_NAME") + "?tls=true"
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
 		log.Println(err)
