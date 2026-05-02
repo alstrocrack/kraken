7adb0be6661eaf48a0cf384a8ed97ba7150a6888
Yuto Urushima <yuto259233@gmail.com>
2026-05-02 21:55:24 +0900

    fix

--- cmd/kraken/main.go
+++ cmd/kraken/main.go
@@ -2,15 +2,23 @@ package main
 
 import (
 	"fmt"
+	"kraken/config/logging"
 	"log"
 	"net/http"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Received request")
+		logging.LoggingInit("../../log.txt")
 		fmt.Fprintf(w, "Hello, World!")
 	})
+
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	log.Println("Starting server on :8080")
 	server := &http.Server{Addr: ":8080"}
--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ go 1.19
 
 require github.com/go-sql-driver/mysql v1.7.1
 
-require golang.org/x/crypto v0.11.0
+require (
+	github.com/joho/godotenv v1.5.1
+	golang.org/x/crypto v0.11.0
+)
